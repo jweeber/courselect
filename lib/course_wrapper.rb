016e66ee86965160979af0958a4e6739e735fef3
Jessica <jessica.weeber@gmail.com>
2016-05-28 08:37:49 -0700

    beginning udacity api call in wrapper. doesn't include query so a little tricky.

--- lib/course_wrapper.rb
+++ lib/course_wrapper.rb
@@ -1,10 +1,30 @@
 require 'httparty'
 
 class	CourseWrapper
-	BASE_URL = "https://api.coursera.org/api/courses.v1"
+	COURSERA_URL = "https://api.coursera.org/api/courses.v1"
+	UDACITY_URL = "https://www.udacity.com/public-api/v0/courses"
+
+	def get_all_courses
+		@coursera
+		@udacity
+		@udemy
+		
+	end
 
 	def self.get_coursera_courses(keyword)
-		HTTParty.get(BASE_URL + "?q=search&query=#{keyword}&includes=instructorIds,primaryLanguages,photoUrl,description&fields=instructorIds,primaryLanguages,photoUrl,description")	
+		@coursera = HTTParty.get(COURSERA_URL + "?q=search&query=#{keyword}&includes=instructorIds,primaryLanguages,photoUrl,description&fields=instructorIds,primaryLanguages,photoUrl,description")	
+	end
+
+	def get_udemy_courses
+		@udemy = 
+	end
+
+	def get_udacity_courses(keyword)
+		@udacity = HTTParty.get(UDACITY_URL)["courses"]
+		@searched_for = []
+		@udacity.each do |course|
+			@searched_for << course if course["title"].include?(keyword)
+		end
 	end
 
 end
