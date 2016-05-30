4a0bd81559e06497d114943d97710dd19f473c50
Jessica <jessica.weeber@gmail.com>
2016-05-30 14:00:54 -0700

    github screwing with my indentation. trying to fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-	has_secure_password
-	
-	validates :username, presence: true, uniqueness: true
+  has_secure_password
+  
+  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: {with: /@/}
 
   def self.log_in(username, password)
--- lib/course_wrapper.rb
+++ lib/course_wrapper.rb
@@ -1,19 +1,19 @@
 require 'httparty'
 
-class	CourseWrapper
-	COURSERA_URL = "https://api.coursera.org/api/courses.v1"
-	UDACITY_URL = "https://www.udacity.com/public-api/v0/courses"
+class CourseWrapper
+  COURSERA_URL = "https://api.coursera.org/api/courses.v1"
+  UDACITY_URL = "https://www.udacity.com/public-api/v0/courses"
 
-	def self.get_coursera_courses(keyword)
-		HTTParty.get(COURSERA_URL + "?q=search&query=#{keyword}&includes=instructorIds,primaryLanguages,photoUrl,description,previewLink&limit=20&fields=instructorIds,primaryLanguages,photoUrl,description,previewLink")["elements"]	
-	end
+  def self.get_coursera_courses(keyword)
+    HTTParty.get(COURSERA_URL + "?q=search&query=#{keyword}&includes=instructorIds,primaryLanguages,photoUrl,description,previewLink&limit=20&fields=instructorIds,primaryLanguages,photoUrl,description,previewLink")["elements"]  
+  end
 
-	# def self.get_udemy_courses
-	# 	@udemy = 
-	# end
+  # def self.get_udemy_courses
+  #   @udemy = 
+  # end
 
-	def self.get_udacity_courses
-		HTTParty.get(UDACITY_URL)["courses"]
-	end
+  def self.get_udacity_courses
+    HTTParty.get(UDACITY_URL)["courses"]
+  end
 
 end
