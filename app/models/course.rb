b71563c4815cd811b6afcf5df145168b9eec5935
Jessica <jessica.weeber@gmail.com>
2016-05-30 14:01:36 -0700

    github screwing with my indentation. trying to fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,29 +1,29 @@
 class Course < ActiveRecord::Base
 
-	# def self.coursera_link(coursera_results)
-	# 	links = []
-	# 	 coursera_results.each do |course|
-	# 		links << "https://www.coursera.org/course/#{course["slug"]}"
-	# 	end
-	# 	return links unless links.nil?
-	# end
+  # def self.coursera_link(coursera_results)
+  #   links = []
+  #    coursera_results.each do |course|
+  #     links << "https://www.coursera.org/course/#{course["slug"]}"
+  #   end
+  #   return links unless links.nil?
+  # end
 
-	# def self.coursera_search(search_results)
-	# 	search_results.each do |course|
-	# 		puts course["name"]
-	# 		puts course
-	# 	end
-		
-	# end
+  # def self.coursera_search(search_results)
+  #   search_results.each do |course|
+  #     puts course["name"]
+  #     puts course
+  #   end
+    
+  # end
 
-	def self.udacity_courses(search_results, keyword)
-			@searched_for = []
-			search_results.each do |course|
-				if course["title"].downcase.include?(keyword.downcase)
-					@searched_for << course
-				end
-		end
-		return @searched_for
-	end
+  def self.udacity_courses(search_results, keyword)
+      @searched_for = []
+      search_results.each do |course|
+        if course["title"].downcase.include?(keyword.downcase)
+          @searched_for << course
+        end
+    end
+    return @searched_for
+  end
 
 end
