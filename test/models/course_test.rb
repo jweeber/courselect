29ea52cedca3abe55883d8b915d7d4db94c6fc63
Jessica <jessica.weeber@gmail.com>
2016-05-30 12:31:52 -0700

    course model testing for udacity search results method created and passing

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@course_search = [{
+  		"title" => "Computer Science 101",
+  		"instructor" => "Jane Smith",
+  		"subtitle" => "Learning Computer Science Basics"
+  	}]
+  	@keyword = "computer science"
+		
+	end
+  test "returns an array of udacity courses" do
+    assert_instance_of Array, Course.udacity_courses(@course_search, @keyword)
+  end
+
+  test "udacity keyword search is case insensitive" do
+  	@search_results = Course.udacity_courses(@course_search, @keyword)
+    assert_equal "Computer Science 101", @search_results[0]["title"]
+  end
 end
