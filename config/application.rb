b6d5f5d7865a15f89fb1cb45d216e50fb6cb2194
Jessica <jessica.weeber@gmail.com>
2016-06-01 11:49:49 -0700

    gem didn't work, adding config to do this manually

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Courselect
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
     config.autoload_paths += %W(#{config.root}/lib)
   end
 end
