a9351461ede8f50951cb4c5f631da0c25341e973
Jessica <jessica.weeber@gmail.com>
2016-06-02 12:14:52 -0700

    going to use khan academy api instead of edx, added description with regex for coursera view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,6 +9,7 @@ class CoursesController < ApplicationController
 	def show
 		keyword = params[:keyword]
 		@coursera_results = CourseWrapper.get_coursera_courses(keyword)
+		@coursera_instructors = CourseWrapper.get_coursera_instructors(keyword)
 		@udacity_search = CourseWrapper.get_udacity_courses
 		@udacity_results = Course.udacity_courses(@udacity_search, keyword)
 	end
--- lib/course_wrapper.rb
+++ lib/course_wrapper.rb
@@ -1,24 +1,42 @@
 require 'httparty'
 
 class CourseWrapper
-  COURSERA_URL = "https://api.coursera.org/api/courses.v1"
+  COURSERA_COURSE_URL = "https://api.coursera.org/api/courses.v1"
+  COURSERA_INSTRUCTOR_URL = "https://api.coursera.org/api/instructors.v1"
   UDACITY_URL = "https://www.udacity.com/public-api/v0/courses"
-  EDX_URL = ""
+  KHAN_URL = "http://www.khanacademy.org/api/v1/topic"
 
   def self.get_coursera_courses(keyword)
-    HTTParty.get(COURSERA_URL + "?q=search&query=#{keyword}&includes=instructorIds,primaryLanguages,photoUrl,description,previewLink&limit=20&fields=instructorIds,primaryLanguages,photoUrl,description,previewLink")["elements"]  
+    HTTParty.get(COURSERA_COURSE_URL + "?q=search&query=#{keyword}&includes=instructorIds,primaryLanguages,photoUrl,description,previewLink&limit=20&fields=instructorIds,primaryLanguages,photoUrl,description,previewLink")["elements"]
   end
 
-  # def self.get_udemy_courses
-  #   @udemy = 
-  # end
+  def self.get_coursera_instructors(keyword)
+    coursera_matches = self.get_coursera_courses(keyword)
+    # instructor ids are in arrays, so this will return an array of arrays
+    instructor_ids = []
+    coursera_matches.each do |course|
+      instructor_ids << course["instructorIds"]
+    end
+
+    instructor_info = []
+    instructor_ids.each do |inst_ids|
+      inst_ids.each do |id|
+        instructor_info << self.get_coursera_instructor_info(id)
+      end
+    end
+    return instructor_info
+  end
+
+  def self.get_coursera_instructor_info(id)
+    HTTParty.get(COURSERA_INSTRUCTOR_URL + "?ids=#{id}&fields=prefixName,title,firstName,lastName") 
+  end
 
   def self.get_udacity_courses
     HTTParty.get(UDACITY_URL)["courses"]
   end
 
-  def self.get_edx_courses
-    HTTParty.get()
+  def self.get_khan_courses(keyword)
+    HTTParty.get(KHAN_URL + "/#{keyword}")["children"]
   end
 
 end
