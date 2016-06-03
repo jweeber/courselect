59224d358a9e145422a98e7362f0d0e7183f58ac
Jessica <jessica.weeber@gmail.com>
2016-06-03 12:06:17 -0700

    fixing automatic weird indentation in controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,22 +3,22 @@ require_relative '../../lib/course_wrapper'
 class CoursesController < ApplicationController
   include CoursesHelper
 
-	def index; end
+  def index; end
 
-	def show
-		keyword = params[:keyword]
-		@coursera_results = CourseWrapper.get_coursera_courses(keyword)
-		@udacity_search = CourseWrapper.get_udacity_courses
-		@udacity_results = udacity_courses(@udacity_search, keyword)
-		@khan_results = CourseWrapper.get_khan_courses(keyword)
-	end
+  def show
+    keyword = params[:keyword]
+    @coursera_results = CourseWrapper.get_coursera_courses(keyword)
+    @udacity_search = CourseWrapper.get_udacity_courses
+    @udacity_results = udacity_courses(@udacity_search, keyword)
+    @khan_results = CourseWrapper.get_khan_courses(keyword)
+  end
 
-	def show_to_take
-		@courses = Course.where(to_take: true)
-	end
+  def show_to_take
+    @courses = Course.where(to_take: true)
+  end
 
-	def show_taken
-		@courses = Course.where(completed: true)
-	end
+  def show_taken
+    @courses = Course.where(completed: true)
+  end
 
 end
